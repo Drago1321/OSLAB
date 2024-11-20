cb5773755765bc4c94e7f9854046be9bae00855c
Drago1321 <sunilshirodkar9998@gmail.com>
2024-11-20 22:13:09 +0530

    Add files via upload

---
+++ PS2.sh
@@ -0,0 +1,13 @@
+# Write a shell Script to find the sum of digits of the entered number. 
+
+echo "Enter a number"
+read number
+
+sum=0
+while [ $number -gt 0 ]; do
+    digit=$((number%10))
+    sum=$((sum+digit))
+    number=$((number/10))
+done
+
+echo "sum of digit is $sum"
